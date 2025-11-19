9c43810b6a2eb48b1931a9955ebe1869b0e04c28
EduSantos <eduardofelipi@gmail.com>
2025-11-19 18:20:31 -0300

    core: fixing babel issue

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"]
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      // NativeWind's Babel helper is a preset (it returns its own plugins)
+      "nativewind/babel"
+    ],
+    // Keep Reanimated last per its setup docs
+    plugins: ["react-native-reanimated/plugin"]
   };
 };
